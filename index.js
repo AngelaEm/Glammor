b77f8efaea18c081230f085aa0d4de08e5167350
Angela Emanuelsson <angelaemanuelsson@gmail.com>
2024-01-23 11:01:51 +0100

    Add new folder with expressAPI

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
 
 // Array with products
-const produkter = [
-    { namn: "Avokado", pris: 50, beskrivning: "Fina hängen som passar i alla lägen.", bild: "Images/avokado.png" },
-    { namn: "Snöflingor", pris: 50, beskrivning: "Snygga snöflingor i vitt och guld.", bild: "Images/snoflinga.png" },
-    { namn: "Spöken", pris: 50, beskrivning: "Söta spöken som finns i svart och vitt.", bild: "Images/spoken.png" },
-    { namn: "Godisbjörnar", pris: 50, beskrivning: "Godisbjörnar som finns i många färger!", bild: "Images/rosabjornar.png" },
-    { namn: "Tomtar", pris: 50, beskrivning: "Såklart man vill ha tomtar i öronen till jul.", bild: "Images/tomtar.png" },
-    { namn: "Planeter", pris: 50, beskrivning: "Svarta planeter som passar till det mesta.", bild: "Images/planeter.png" },
-    { namn: "Blixtar", pris: 50, beskrivning: "Coola blixtformade örhängen. Finns i olika färger.", bild: "Images/blixtar.png" },
-    { namn: "Hundtassar", pris: 50, beskrivning: "Söta hundtassar till hundälskaren.", bild: "Images/tassar.png" },
-];
+const products = [];
+addProductsToArray();
+
+// Get all products and add products to array 
+function addProductsToArray(){
+    getAllProducts().then(data => {
+        products.push(...data); 
+        console.log(products); 
+        }).catch(error => {
+            console.error("Det gick inte att hämta produkterna:", error);
+    });
+}
+
+// Get products from database
+function getAllProducts(){
+    return fetch("http://localhost:3000/earrings")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Nätverksrespons var inte ok");
+        }
+        return response.json();
+    })
+    .catch(error => {
+        console.error("Problem med att hämta produkter:", error);
+        throw error; 
+    });
+}
 
 // Show products
-visaProdukter();
+showProducts();
 
 // Function to show all products as a card with name, photo, description, price and add-button.
-function visaProdukter() {
+function showProducts() {
     const container = document.getElementById('produkterContainer');
     container.innerHTML = '';
     
-    produkter.forEach(produkt => {
-        const kort = `
+    getAllProducts().then(produkter => {
+        produkter.forEach(produkt => {
+            const kort = `
             <div class="col-sm-6 col-md-4 col-lg-3">
                 <div class="card">
-                    <img src="${produkt.bild}" class="card-img-top" alt="${produkt.namn}">
+                    <img src="${produkt.image}" class="card-img-top" alt="${produkt.name}">
                     <div class="card-body">
-                        <h5 class="card-title">${produkt.namn}</h5>
-                        <p class="card-text">${produkt.beskrivning}</p>
-                        <a href="javascript:void(0);" onclick="laggTillIKundvagn('${produkt.namn}')" class="btn btn-light">Lägg till</a>
+                        <h5 class="card-title">${produkt.name}</h5>
+                        <p class="card-text">${produkt.description}</p>
+                        <p class="card-text">Pris: ${produkt.price} kr</p>
+                        <p class="card-text">Antal: ${produkt.quantity} i lager</p>
+                        <a href="javascript:void(0);" onclick="laggTillIKundvagn('${produkt.name}')" class="btn btn-light">Lägg till</a>
                     </div>
                 </div>
             </div>
         `;
         container.innerHTML += kort;
     });
+}).catch(error => {
+    container.innerHTML = '<p>Problem med att ladda sidan</p>';
+    console.error(error);
+});
 }
 
 // Function to update cart
@@ -43,7 +67,7 @@ function uppdateraKundkorg() {
 
     kundvagn.forEach(produkt => {
         const li = document.createElement('li');
-        li.innerText = `${produkt.namn} - ${produkt.pris} kr`; 
+        li.innerText = `${produkt.name} - ${produkt.price} kr`; 
         li.className = 'list-group-item';
         kundkorgLista.appendChild(li);
     });
@@ -55,12 +79,12 @@ let kundvagn = [];
 
 // Add to cart function
 function laggTillIKundvagn(produktNamn) {
-    const produktIndex = kundvagn.findIndex(item => item.produkt.namn === produktNamn);
+    const produktIndex = kundvagn.findIndex(item => item.produkt.name === produktNamn);
     if (produktIndex > -1) {
         kundvagn[produktIndex].antal +=1;
     }
     else{
-        const produkt = produkter.find(p => p.namn === produktNamn);
+        const produkt = products.find(p => p.name === produktNamn);
         if(produkt){
             kundvagn.push({produkt, antal: 1});
         }  
@@ -76,24 +100,24 @@ function visaKundkorg() {
     let totalSumma = 0;
     
     kundvagn.forEach(item => {
-        const totalPris = item.produkt.pris * item.antal;
+        const totalPris = item.produkt.price * item.antal;
         totalSumma += totalPris;
         const vara = `
             <div class="row mb-2 align-items-center">   
                 <div class="col-2">
-                    <img src="${item.produkt.bild}" alt="${item.produkt.namn}" class="img-fluid">
+                    <img src="${item.produkt.image}" alt="${item.produkt.name}" class="img-fluid">
                 </div>
                 <div class="col-3">
-                    <span>${item.produkt.namn}</span>
+                    <span>${item.produkt.name}</span>
                 </div>
                 <div class="col-2">
                     <input type="number" class="form-control" value="${item.antal}" min="1" onchange="andraAntal('${item.produkt.namn}', this.value)">
                 </div>
                 <div class="col-3">
-                    <span>Pris: ${item.produkt.pris} kr/st</span>
+                    <span>Pris: ${item.produkt.price} kr/st</span>
                 </div>
                 <div class="col-2">
-                    <button class="btn btn-danger" onclick="taBortUrKundvagn('${item.produkt.namn}')">Ta bort</button>
+                    <button class="btn btn-danger" onclick="taBortUrKundvagn('${item.produkt.name}')">Ta bort</button>
                 </div>
             </div>
         `;
@@ -105,7 +129,7 @@ function visaKundkorg() {
 
 // Change number of items in cart-function
 function andraAntal(produktNamn, nyttAntal) {
-    const produktIndex = kundvagn.findIndex(item => item.produkt.namn === produktNamn);
+    const produktIndex = kundvagn.findIndex(item => item.produkt.name === produktNamn);
     if (produktIndex > -1) {
         kundvagn[produktIndex].antal = parseInt(nyttAntal);
         uppdateraKundkorg();
@@ -115,13 +139,34 @@ function andraAntal(produktNamn, nyttAntal) {
 
 // Delete items from cart-function
 function taBortUrKundvagn(produktNamn) {
-    const produktIndex = kundvagn.findIndex(item => item.produkt.namn === produktNamn);
+    const produktIndex = kundvagn.findIndex(item => item.produkt.name === produktNamn);
     if (produktIndex > -1) {
         kundvagn.splice(produktIndex, 1);
         uppdateraKundkorg();
         visaKundkorg();
     }
 }
+
+// Funktion to update
+function uppdateraProduktIAPi(produktId, uppdateradProdukt) {
+    return fetch(`http://localhost:3000/earring/${produktId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(uppdateradProdukt),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Nätverksrespons var inte ok');
+        }
+        return response.json();
+    })
+    .catch(error => {
+        console.error('Problem med att uppdatera produkten:', error);
+    });
+}
+
 
 
 // Display cart
@@ -135,10 +180,20 @@ function clearKundkorg(){
     uppdateraKundkorg();
 }
 
-// Message "Köp genomfört"
-function buy(){
-    alert("Köp genomfört!")
-    kundvagn=[];
+function buy() {
+    kundvagn.forEach(item => {
+        const nyQuantity = item.produkt.quantity - item.antal;
+        if (nyQuantity > 0) {
+            uppdateraProduktIAPi(item.produkt.id, { ...item.produkt, quantity: nyQuantity });
+            alert("Köp genomfört!");
+            getAllProducts();
+            showProducts();
+        } else {
+            alert("Inte tillräckligt med produkter i lager");
+        }
+    });
+
+    kundvagn = [];
     uppdateraKundkorg();
 }
 
